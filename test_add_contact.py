d2ebf56aefbafd8bd4bd4840c2e31bd51abcf96f
mglowacki <m.glowacki@merixstudio.com>
2018-12-19 20:56:42 +0100

    Add help methods

--- test_add_contact.py
+++ test_add_contact.py
@@ -11,14 +11,19 @@ class TestAddContact(unittest.TestCase):
     
     def test_add_contact(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost/addressbook/index.php")
-        # login
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.create_contact(wd)
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self, wd):
+        wd.find_element_by_link_text("home").click()
+
+    def create_contact(self, wd):
         # init contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
@@ -68,10 +73,16 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("notes").send_keys("TestNotes")
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        # return to home page
-        wd.find_element_by_link_text("home").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
 
     def tearDown(self):
         self.wd.quit()
