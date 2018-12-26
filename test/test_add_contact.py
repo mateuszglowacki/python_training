f8f950d86f4444c4cb70afd3f611dab4aa2eaa73
mglowacki <m.glowacki@merixstudio.com>
2018-12-26 21:53:40 +0100

    Extract class for group and contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login("admin", "secret")
-    app.create_contact(Contact("TestName", "TestMiddleName", "TestLastName", "TestNickName", "TestTitle",
+    app.contact.create(Contact("TestName", "TestMiddleName", "TestLastName", "TestNickName", "TestTitle",
         "TestCompany", "TestAddress", "111111111", "222222222", "333333333", "444444444",
         "testmail1@test.com", "testmail2@test.com", "testmail3#test.com", "test.com", "1",
         "January", "1990", "1", "February", "2000", "TestNdAddress", "TestHome", "TestNotes"))
