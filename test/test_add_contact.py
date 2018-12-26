41eaa38a3a8b6bff53f1e1f17d14549abcb0f359
mglowacki <m.glowacki@merixstudio.com>
2018-12-26 21:38:55 +0100

    Helper class for session

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,9 +12,9 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     app.create_contact(Contact("TestName", "TestMiddleName", "TestLastName", "TestNickName", "TestTitle",
         "TestCompany", "TestAddress", "111111111", "222222222", "333333333", "444444444",
         "testmail1@test.com", "testmail2@test.com", "testmail3#test.com", "test.com", "1",
         "January", "1990", "1", "February", "2000", "TestNdAddress", "TestHome", "TestNotes"))
-    app.logout()
+    app.session.logout()
