e6860bc70e5dc12acf5579fd72b62b79c3528201
mglowacki <m.glowacki@merixstudio.com>
2019-01-02 22:43:09 +0100

    Delete/Modify tests improvements - verify preconditions

--- test/contact/test_modify_contact.py
+++ test/contact/test_modify_contact.py
@@ -2,6 +2,8 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact("EditName", "EditMiddleName", "EditLastName", "EditNickName", "EditTitle",
         "EditCompany", "EditAddress", "111111111", "222222222", "333333333", "444444444",
         "editmail1@test.com", "editmail2@test.com", "editmail3@test.com", "edit.com", "1",
@@ -9,8 +11,12 @@ def test_modify_first_contact(app):
 
 
 def test_modify_contact_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="New name"))
 
 
 def test_modify_contact_title(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(title="New title"))
