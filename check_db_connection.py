b7c45987c459f901bb9ab4746edf83be537fde63
mglowacki <m.glowacki@merixstudio.com>
2019-01-31 22:07:08 +0100

    Add ORM

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,21 +1,12 @@
-#import mysql.connector
-#import pymysql.cursors
-from fixture.db import DbFixture
+from fixture.orm import ORMFixture
 
-#connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
-#connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 
 try:
-#    cursor = connection.cursor()
-#    cursor.execute("select * from group_list")
-#    for row in cursor.fetchall():
-#       print(row)
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-#    connection.close()
-    db.destroy()
+    pass # db.destroy()
